7c5a12f351002b3794101e99edc58bbed2d7a22a
Unai.m <unaim.cebanc@gmail.com>
2026-04-24 09:47:11 +0200

    Añadido index.html

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = "ABCD"
+
+@app.route("/")
+def home():
+    session.clear()
+    return render_template("index.html")
+
 def main():
-    print("Hello from github!")
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
