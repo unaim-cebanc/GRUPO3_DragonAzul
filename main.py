18c19fd6d85ea01b480caf3a9f7fd09e59ed0739
Unai.m <unaim.cebanc@gmail.com>
2026-04-27 17:00:52 +0200

    Añadida función de inicio de sesion

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import sql_scripts
 
 app = Flask(__name__)
 app.secret_key = "ABCD"
 
+# Recuerda: 'py main.py' para ejecutar el servidor local y poder hacer pruebas ;)
+
 @app.route("/")
 def home():
-    session.clear()
     return render_template("index.html")
+
+@app.route("/inicio_sesion", methods = ['GET', 'POST'])
+def inicio_sesion():
+    if request.method == 'POST':
+        usuario = request.form.get("usuario")
+        psw = request.form.get("contrasenia")
+        
+        if sql_scripts.validate_user(usuario, psw):
+            session["usuario"] = usuario
+            return redirect(url_for("home")) # redirección placeholder
+    return render_template("inicio_sesion.html")
 
 def main():
     app.run(debug=True)
-
 
 if __name__ == "__main__":
     main()
