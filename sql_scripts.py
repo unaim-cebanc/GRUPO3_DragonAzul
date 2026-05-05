d4cd6fd9379da629211f238cb016e97995713016
Perugainza <gainzaperu1990@gmail.com>
2026-05-05 11:00:24 +0200

    Desencriptacion

--- sql_scripts.py
+++ sql_scripts.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
+import bcrypt
 
 # Realiza la conexión con la base de datos
 def try_conn():
@@ -55,15 +56,105 @@ def is_admin(user):
         return False
     return True
 
-if __name__ == "__main__":
+def get_user_id(user):
+    CONEXION = try_conn()
+    CURSOR = CONEXION.cursor()
     
-    usuario = input("Introduce tu nombre de usuario: ").strip().lower()
-    contraseña = input("Introduce tu contraseña: ").strip()
+    query = "SELECT id_usuario FROM usuario WHERE nombre = %s"
+    values = (user,)
     
-    if validate_user(usuario, contraseña):
-        if is_admin(usuario):
-            print("Sesion iniciada como admin")
-        else:
-            print("Sesion iniciada como invitado")
+    CURSOR.execute(query, values)
+    result = CURSOR.fetchone()
+    close_conn(CONEXION, CURSOR)
+    
+    if result is None:
+        print("Usuario no encontrado!")
+        return
+    return int(result[0])
+
+def fetch_recepies_info():
+    CONEXION = try_conn()
+    CURSOR = CONEXION.cursor()
+    
+    query = """
+    SELECT r.nombre, r.imagen, r.tipo, u.nombre
+    FROM receta r INNER JOIN usuario u ON r.id_usuario = u.id_usuario
+    """
+    CURSOR.execute(query)
+    result = CURSOR.fetchall()
+    close_conn(CONEXION, CURSOR)
+    
+    if result is None:
+        return
+    return result
+
+def insert_recepie(nombre, img, desc, ingredientes, cantidades, unidades, pasos, tiempo_prep, tipo, user_id):
+    CONEXION = try_conn()
+    CURSOR = CONEXION.cursor()
+    
+    query_receta = """
+    INSERT INTO receta (nombre, imagen, descripcion, tiempo_preparacion, tipo, id_usuario) VALUES
+    (%s, %s, %s, %s, %s, %s)
+    """
+    values_receta = (nombre, img, desc, tiempo_prep, tipo, user_id)
+    
+    CURSOR.execute(query_receta, values_receta)
+    CONEXION.commit()
+    
+    query_id = "SELECT id_receta FROM receta WHERE nombre = %s AND id_usuario = %s"
+    values_id = (nombre, user_id)
+    CURSOR.execute(query_id, values_id)
+    id_receta = CURSOR.fetchone()[0]
+    CURSOR.fetchall()
+    
+    query_ingredientes = """
+    INSERT INTO receta_ingrediente (id_receta, nombre, cantidad, unidad) VALUES (%s, %s, %s, %s)
+    """
+    values_ingredientes = [(id_receta, ing, qty, uni) for ing, qty, uni in zip(ingredientes, cantidades, unidades)]
+    
+    CURSOR.executemany(query_ingredientes, values_ingredientes)
+    CONEXION.commit()
+    
+    query_pasos = """
+    INSERT INTO pasos (id_receta, paso) VALUES (%s, %s)
+    """
+    values_pasos = [(id_receta, paso) for paso in pasos]
+    
+    CURSOR.executemany(query_pasos, values_pasos)
+    CONEXION.commit()
+    
+    close_conn(CURSOR, CONEXION)
+    print("datos insertados!")
+
+def register_user(nombre, email, contrasena):
+    CONEXION = try_conn()
+    CURSOR = CONEXION.cursor()
+    
+    query = """
+        INSERT INTO usuario (nombre, email, contrasena) VALUES (%s, %s, %s)
+    """
+    secure_psw = encrypt_password(contrasena)
+    values = (nombre, email, secure_psw)
+    
+    CURSOR.execute(query, values)
+    CONEXION.commit()
+    print("Usuario registrado!")
+    close_conn(CURSOR, CONEXION)
+
+def encrypt_password(psw):
+    salt = bcrypt.gensalt()
+    psw_hash = bcrypt.hashpw(psw.encode("utf-8"), salt)
+    return psw_hash
+
+def decrypt_password(psw, hash):
+    if bcrypt.checkpw(psw.encode("utf-8"),hash):
+        return True
     else:
-        print("Credenciales incorrectas")
+        return False
+    
+
+if __name__ == "__main__":
+    usuario = input("usuario: ")
+    psw = input("contraseña: ")
+    email = input("email: ")
+    register_user(usuario, email, psw)
